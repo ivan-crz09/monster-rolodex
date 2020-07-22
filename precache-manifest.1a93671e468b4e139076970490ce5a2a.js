71d992fb74e38c0545273744c4149f2532d77204
Iván Cruz <ivan.crz09@gmail.com>
2020-07-21 19:39:36 -0500

    Updates

---
+++ precache-manifest.1a93671e468b4e139076970490ce5a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a571d3d176daa884b16e6c18e31825",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e544698f36dc69db0a1f",
+    "url": "/monster-rolodex/static/css/main.9560740a.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e544698f36dc69db0a1f",
+    "url": "/monster-rolodex/static/js/main.3aa2f738.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
